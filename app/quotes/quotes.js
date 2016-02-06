15807147a34b116bdee65e21a6d5acc7e34793fe
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-02-06 12:07:38 -0700

    working out kink

--- app/quotes/quotes.js
+++ app/quotes/quotes.js
@@ -10,5 +10,55 @@ angular.module('myInspiration.quotes', ['ngRoute','firebase'])
 }])
 
 .controller('QuotesCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-console.log($scope);
+    var ref = new Firebase('https://inspirationalquotes.firebaseio.com/quotes');
+    
+    //init firebase
+    $scope.quotes = $firebaseArray(ref);
+
+    //click add button - show form and clear past data
+    $scope.showAddForm = function(){
+        $scope.addFormShow = true;
+    };
+    
+    //hide upon clicking
+    $scope.hide = function(){
+        $scope.addFormShow = false;
+    };
+    
+    //must have value for all variable, even if null - setting to null if none, for firebase
+    $scope.addFormSubmit = function(){
+        
+        //asign values
+        if ($scope.author){var author = $scope.author;}
+        else {var author = null;}
+        if ($scope.text){var text = $scope.text;}
+        else {var text = null;}
+        if ($scope.subject){var subject = $scope.subject;}
+        else {var subject = null;}
+        if ($scope.rating){var rating = $scope.autho;r}
+        else {var rating = null;}
+        
+        //build object
+        $scope.quotes.$add({
+            author:author,
+            text: text,
+            subject: subject,
+            rating: rating
+            
+        }).then(function(ref){
+            //assign primary key
+            var id = ref.key();
+            console.log("added quote with id" + id);
+            
+            //clear form
+            clearFiends();
+            
+            //hide form
+            $scope.addFormShow = false;
+            
+            //send message
+         $scope.msg = 'Quote added';
+            
+        });
+            
 }]);
